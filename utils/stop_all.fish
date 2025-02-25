ad73f522925767aa6825addaa9c3d01f868ce855
xstar97 <dev.xstar97@gmail.com>
2025-02-25 01:28:44 +0000

    update

--- utils/stop_all.fish
+++ utils/stop_all.fish
@@ -1,27 +1,47 @@
-# stop_all function to toggle the 'stopAll' value in HelmRelease
 function stop_all
     check_command "kubectl"
+    
     # Ensure chart name is provided
     if test (count $argv) -lt 1
-        echo "Usage: stop_all <chart-name> -n <namespace>"
+        echo "Usage: stop_all <chart-name> -n <namespace> [--status]"
         return 1
     end
 
     set CHART_NAME $argv[1]
     set NAMESPACE $CHART_NAME
 
-    # Parse namespace flag if provided
+    # Parse flags
+    set status_flag 0
+
     for i in (seq 2 (count $argv))
         switch $argv[$i]
-        case '-n'
-            set NAMESPACE $argv[(math $i + 1)]
-            break
+            case '-n'
+                set NAMESPACE $argv[(math $i + 1)]
+                break
+            case '--status'
+                set status_flag 1
+            case '*'
+                echo "Invalid argument: $argv[$i]"
+                return 1
         end
     end
 
-    echo "...checking state"
+    # If --status is set, check current state
+    if test $status_flag -eq 1
+        echo "...checking state"
+        set CURRENT_STATE (kubectl get helmrelease $CHART_NAME -n $NAMESPACE -o jsonpath='{.spec.values.global.stopAll}' 2>/dev/null)
+        
+        if test -z "$CURRENT_STATE"
+            echo "Error: Could not retrieve stopAll value for $CHART_NAME in namespace $NAMESPACE"
+            return 1
+        end
 
-    # Get current stopAll value
+        echo "stopAll is $CURRENT_STATE"
+        return 0
+    end
+
+    # Get current stopAll value and toggle it
+    echo "...checking current state"
     set CURRENT_STATE (kubectl get helmrelease $CHART_NAME -n $NAMESPACE -o jsonpath='{.spec.values.global.stopAll}' 2>/dev/null)
 
     if test -z "$CURRENT_STATE"
@@ -29,7 +49,7 @@ function stop_all
         return 1
     end
 
-    echo "stopAll is $CURRENT_STATE"
+    echo "Current stopAll is $CURRENT_STATE"
 
     # Toggle stopAll value
     set NEW_STATE "true"
@@ -37,9 +57,9 @@ function stop_all
         set NEW_STATE "false"
     end
 
-    echo "setting stopAll to $NEW_STATE"
+    echo "Setting stopAll to $NEW_STATE"
 
-    # Apply the patch
+    # Apply the patch to toggle stopAll value
     kubectl patch helmrelease $CHART_NAME -n $NAMESPACE --type='merge' -p \
       "{\"spec\":{\"values\":{\"global\":{\"stopAll\":$NEW_STATE}}}}"
 
